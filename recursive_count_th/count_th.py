c4ecbdaeae9f4fb1e76dbe89c378b5d15bb1b94d
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-01-25 11:22:45 -0500

    Recursive algo refactored to O(n)

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,39 +3,20 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-count = 0
+
+# Recursive algo runs at O(n)
 
 def count_th(word):
-    global count
 
-    if len(word) < 1:
-       return 0
+    if len(word) <= 1:
+        return 0
 
-    left, right = word[:len(word)//2], word[len(word)//2 if len(word)%2 == 0 else ((len(word)//2)+1) - 1:]
+    word_list = list(word)
 
-    if left.endswith('t') and right.startswith('h'):
-        count += 0.5
-    elif left == 'th':
-        count += 1
-    elif len(left) == 1:
-        return
+    if word_list[0] == 't' and word_list[1] == 'h':
+        word_list.pop(0)
+        return 1 + count_th(''.join(word_list))
     else:
-        count_th(left)
+        word_list.pop(0)
+        return 0 + count_th(''.join(word_list))
 
-    if left.endswith('t') and right.startswith('h'):
-        count += 0.5  
-    elif right == 'th':
-        count += 1
-    elif len(right) == 1:
-        return  
-    else:
-        count_th(right)
-
-    print(int(count)) 
-    return count
-
-
-
-test_word = "THtHThth"
-
-count_th(test_word)
